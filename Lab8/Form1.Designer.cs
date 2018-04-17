9f93d4cdeb8f08264e4184598edd70248edb3c00
paradise999 <32838010+paradise999@users.noreply.github.com>
2018-04-18 02:42:32 +0300

    Initial commit

---
+++ Lab8/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.screenshot = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(68, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(729, 469);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // screenshot
+            // 
+            this.screenshot.Location = new System.Drawing.Point(887, 556);
+            this.screenshot.Name = "screenshot";
+            this.screenshot.Size = new System.Drawing.Size(155, 23);
+            this.screenshot.TabIndex = 1;
+            this.screenshot.Text = "Сделать снимок";
+            this.screenshot.UseVisualStyleBackColor = true;
+            this.screenshot.Click += new System.EventHandler(this.screenshot_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(887, 450);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(155, 23);
+            this.save.TabIndex = 2;
+            this.save.Text = "Сохранить снимок";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 605);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.screenshot);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Скриншот";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button screenshot;
+        private System.Windows.Forms.Button save;
+    }
+}
+
---
+++ Lab8/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private Bitmap TakeScreenShot(Screen currentScreen)
+        {
+            Bitmap bmpScreenShot = new Bitmap(currentScreen.Bounds.Width,
+                                              currentScreen.Bounds.Height,
+                                              System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            Graphics gScreenShot = Graphics.FromImage(bmpScreenShot);
+
+            gScreenShot.CopyFromScreen(currentScreen.Bounds.X,
+                                       currentScreen.Bounds.Y,
+                                       0, 0,
+                                       currentScreen.Bounds.Size,
+                                       CopyPixelOperation.SourceCopy);
+            return bmpScreenShot;
+        }
+
+        private Bitmap bmp;
+        private void screenshot_Click(object sender, EventArgs e)
+        {
+
+            // это действие задаёт параметр расположения
+            //изображения в pictureBox1
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            //Делаем снимок экрана (изображение в памяти)
+            Image pr = TakeScreenShot(Screen.PrimaryScreen);
+
+            //Создание экземпляра класса Bitmap, в котом  
+            //будет короткое время храниться наше изображение
+            bmp = new Bitmap(pr);
+
+            //Впихиваем изображение в pictureBox1
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save("скриншот.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+}
